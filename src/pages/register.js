766fbb10f6b280c50d7b8f2e89c1a855d9a73f35
kholostyak17 <kholostyak17@gmail.com>
2021-09-08 22:16:46 +0000

    Made register page & add register button in login

--- src/pages/register.js
+++ src/pages/register.js
@@ -2,25 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom"
 import { useForm } from "react-hook-form";
 
-const Login = () => {
+const Register = () => {
     const { register, handleSubmit } = useForm();
     const onSubmit = (data) => alert(JSON.stringify(data));
 
     return (
-            <div className="div-login">
-                <h3 className="primary-color">Iniciar sesión</h3>
+        <div className="container">
+            <div className="col-12 col-md-5 m-3">
+                <h2 className="primary-color">Registro</h2>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="div-login-form mb-3">
+                    <div className="div-register-form mb-3">
+                        <label htmlFor="name" className="form-label mt-3">Nombre</label>
+                        <input {...register("name")} type="text" name="name" className="form-control" id="name" />
                         <label htmlFor="email" className="form-label mt-3">Correo electrónico</label>
                         <input {...register("email")} type="email" name="email" className="form-control" id="email" />
                         <label htmlFor="password" className="form-label mt-3">Contraseña</label>
                         <input {...register("password")} type="password" name="password" className="form-control" id="password" />
+                        <div className="text-center">
+                            <input type="submit" value="Registrarse" className="button-green m-5" />
+                            <p>¿Ya tienes cuenta? <Link to="login"><span className="primary-color">Inicia sesión</span></Link></p>
+                        </div>
                     </div>
-                    <input type="submit" value="Acceder" className="button-green" />
                 </form>
-                <p>¿No tienes cuenta? <Link to="register"><span className="primary-color">Regístrate</span></Link></p>
             </div>
+        </div>
     );
 };
 
-export default Login;
+export default Register;
