994d648446705bfe7d8dfd2174c4f470c4d3b467
kholostyak17 <kholostyak17@gmail.com>
2021-09-09 01:29:46 +0000

    Implement router in app

--- src/App.js
+++ src/App.js
@@ -3,29 +3,39 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MyNavbar from "./components/my-navbar";
 import Wallet from "./pages/wallet";
+import Transfer from "./pages/transfer";
+import AddMoney from "./pages/add-money";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
-  return (
-    <div className="main-div">
-      <BrowserRouter>
-        <MyNavbar/>
+	return (
+		<div className="div-app">
+			<BrowserRouter>
+				<MyNavbar />
 				<Switch>
-					<Route exact path="/profile">
+					<Route exact path="/wallet">
 						<Wallet />
 					</Route>
-          <Route exact path="/login">
-						{/* <Login /> */}
+					<Route exact path="/transfer">
+						<Transfer />
 					</Route>
-          <Route exact path="/register">
-						{/* <Register /> */}
+					<Route exact path="/add-money">
+						<AddMoney />
+					</Route>
+					<Route exact path="/login">
+						<Login />
+					</Route>
+					<Route exact path="/register">
+						<Register />
 					</Route>
 					<Route>
 						<h1>Not found!</h1>
 					</Route>
 				</Switch>
 			</BrowserRouter>
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
