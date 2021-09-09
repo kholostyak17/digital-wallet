1d72eaa8fd7762ee4f7d9a2d6470588f706ad1c0
kholostyak17 <kholostyak17@gmail.com>
2021-09-09 01:32:30 +0000

    Implement login/logout button in MyNavbar componen

--- src/components/my-navbar.js
+++ src/components/my-navbar.js
@@ -5,16 +5,27 @@ import Nav from 'react-bootstrap/Nav';
 const MyNavbar = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="my-navbar px-3">
-                <Navbar.Brand href="">Digital Wallet</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="profile">Mi perfil</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="login">Iniciar sesión</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+            <Navbar.Brand href="">Digital Wallet</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="wallet">Mi cartera</Nav.Link>
+                    <Nav.Link href="add-money">Añadir dinero</Nav.Link>
+                    <Nav.Link href="transfer">Transferir</Nav.Link>
+                </Nav>
+                <Nav>
+                    {localStorage.getItem("signedIn") == "hola"
+                        ?
+                        (<Nav.Link href="login">Iniciar sesión</Nav.Link>)
+                        :
+                        (<Nav.Link href="/">
+                            <span onClick={localStorage.clear} className="text-danger" >
+                                Cerrar sesión
+                            </span>
+                        </Nav.Link>)
+                    }
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     );
 };
