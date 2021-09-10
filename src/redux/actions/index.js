ef2c8de4827f96a12759ebbd1b703a448c78cbf2
kholostyak17 <kholostyak17@gmail.com>
2021-09-10 11:52:15 +0000

    ADDMONEY FEATURE WORKS !!! in reducer

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,10 +1,18 @@
-import { SIGNUP_USER, LOGIN_USER } from "./types"
+import { SIGNUP_USER, LOGIN_USER, ADD_MONEY, GET_NEW_DATE, MAKE_TRANSFERENCE} from "./types"
 
-export const signUpUserAction = user => ({
+export const signUpUserAction = newAccountData => ({
     type: SIGNUP_USER,
-    payload: user,
+    payload: newAccountData,
 });
 export const loginUserAction = credentials => ({
     type: LOGIN_USER,
     payload: credentials,
+});
+export const addMoneyAction = data => ({
+    type: ADD_MONEY,
+    payload: data,
+});
+export const getNewAction = () => ({
+    type: GET_NEW_DATE,
+    payload: undefined,
 });
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,4 +1,4 @@
-import { SIGNUP_USER, LOGIN_USER } from "../actions/types";
+import { SIGNUP_USER, LOGIN_USER, ADD_MONEY, GET_NEW_DATE} from "../actions/types";
 
 const userReducer = (state, action) => {
     switch (action.type) {
@@ -10,11 +10,45 @@ const userReducer = (state, action) => {
         case LOGIN_USER:
             return {
                 ...state,
-                users: state.users.filter(email => email)
+                users: state.users
             };
+        case ADD_MONEY:
+            console.log(state);
+            const data = action.payload;
+            let account = state.users.find(elem => elem.email === data.email); //guardo objeto con datos de 1 usuario
+            const position = state.users.indexOf(account);  //busco la posición en la que se encuentra de toda la lista
+            const idTransaction = 1 //getNewID();   //me devuelve un ID, e incrementa el valor para siguiente llamada
+            const dateTransaction = userReducer(state, {type:"GET_NEW_DATE"});   //devuelve fecha
+            const newTransaction = {    //genero nuevo objeto transacción de tipo depósito
+                type: "deposit",
+                id_transaction: parseInt(idTransaction),
+                beneficiaryEmail: data.email,
+                beneficiaryName: account.name,
+                amount: parseInt(data.amount),
+                balance: parseInt(account.money) + parseInt(data.amount),
+                date: dateTransaction,
+            };
+            console.log(newTransaction, typeof data.amonut, typeof account.money);
+            console.log(account);
+            account.money = parseInt(account.money) + parseInt(data.amount);
+            account.transactions.push(newTransaction);  //meto última transacción en la lista de transacciones
+            const newState = state.users;
+            newState[position] = account; //introduzco el objeto modificado con la ultima transacción del usuario
+            localStorage.setItem("data",JSON.stringify(newState)); //guardo en localstorage una copia del estado
+            return {
+                ...state,
+                users: newState,
+            };
+        case GET_NEW_DATE:
+            const d = new Date();
+            return (d.getDate().toString().concat("/", d.getMonth().toString(), "/", d.getFullYear().toString(),
+                    " ", d.getHours().toString(), ":", d.getMinutes().toString()));
         default:
             return state;
     }
 };
 
 export default userReducer;
+
+
+//global functions
