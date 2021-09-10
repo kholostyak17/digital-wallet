d2b6a154374bbe9456bbb39a542b353071646ac4
kholostyak17 <kholostyak17@gmail.com>
2021-09-10 11:45:26 +0000

    Add seeddata to initialSt& take from localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,22 +2,68 @@ import { createStore } from 'redux';
 import reducer from "./reducers";
 
 const initialState = {
-    users: [
-        {
-            id:0,
-            email:"default@email.com",
-            password:"default",
-            name: "default",
-            money: 50
-        },
-        {
-            id:1,
-            email:"test@email.com",
-            password:"test",
-            name: "test",
-            money: 50
-        },
-    ]
+    users: (
+        localStorage.getItem("data")
+            ?
+            JSON.parse(localStorage.getItem("data"))
+            :
+            [
+                {
+                    id: 0,
+                    email: "blue@email.com",
+                    password: "blue",
+                    name: "blue",
+                    money: 380,
+                    transactions: [
+                        {
+                            type: "deposit",
+                            id_transaction: 0,
+                            beneficiaryEmail: "blue@email.com",
+                            beneficiaryName: "blue",
+                            amount: 500,
+                            balance: 500,
+                        },
+                        {
+                            type: "transference",
+                            id_transaction: 2,
+                            senderEmail: "blue@email.com",
+                            senderName: "blue",
+                            receptorEmail: "green@email.com",
+                            receptorName: "green",
+                            amount: -120,
+                            balance: 380,
+                        }
+                    ],
+                },
+                {
+                    id: 1,
+                    email: "green@email.com",
+                    password: "green",
+                    name: "green",
+                    money: 620,
+                    transactions: [
+                        {
+                            type: "deposit",
+                            id_transaction: 1,
+                            beneficiaryEmail: "green@email.com",
+                            beneficiaryName: "green",
+                            amount: 500,
+                            balance: 500,
+                        },
+                        {
+                            type: "transference",
+                            id_transaction: 2,
+                            senderEmail: "blue@email.com",
+                            senderName: "blue",
+                            receptorEmail: "green@email.com",
+                            receptorName: "green",
+                            amount: 120,
+                            balance: 620,
+                        }
+                    ],
+                },
+            ]
+    )
 }
 
 export const store = createStore(
