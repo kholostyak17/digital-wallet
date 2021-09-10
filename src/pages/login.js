d8df53cdb22867150116884ba0bd74afd24b27ed
kholostyak17 <kholostyak17@gmail.com>
2021-09-10 09:38:42 +0000

    Save credential vars in localStorage when sign in

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,26 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom"
 import { useForm } from "react-hook-form";
+import { useSelector } from "react-redux";
+import { browserRouterRef } from "../App";
+import MyNavbar from "../components/my-navbar";
+//import { loginUserAction } from "../redux/actions";
 
 const Login = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = (data) => alert(JSON.stringify(data));
+    const users = useSelector(state => state.users);
 
-    return (
-            <div className="div-login">
-                <h3 className="primary-color">Iniciar sesión</h3>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="div-login-form mb-3">
-                        <label htmlFor="email" className="form-label mt-3">Correo electrónico</label>
-                        <input {...register("email")} type="email" name="email" className="form-control" id="email" />
-                        <label htmlFor="password" className="form-label mt-3">Contraseña</label>
-                        <input {...register("password")} type="password" name="password" className="form-control" id="password" />
-                    </div>
-                    <input type="submit" value="Acceder" className="button-green" />
-                </form>
-                <p>¿No tienes cuenta? <Link to="register"><span className="primary-color">Regístrate</span></Link></p>
-            </div>
-    );
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        const userAccount = users.find(element => element.email === data.email);
+        console.log(users, data, userAccount);
+        if (userAccount && data.password === userAccount.password) {
+            localStorage.setItem("name", userAccount.name);
+            localStorage.setItem("email", userAccount.email);
+            localStorage.setItem("signedIn", true);
+            browserRouterRef.current.history.replace("/wallet");
+        }
+        else {
+            localStorage.clear();
+            alert("Usuario no encontrado / Credenciales incorrectas")
+        }
+    };
+
+    return (<>
+        <MyNavbar />
+        <div className="div-login">
+            <h3 className="primary-color">Iniciar sesión</h3>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="div-login-form mb-3">
+                    <label htmlFor="email" className="form-label mt-3">Correo electrónico</label>
+                    <input {...register("email")} type="email" name="email" className="form-control" id="email" />
+                    <label htmlFor="password" className="form-label mt-3">Contraseña</label>
+                    <input {...register("password")} type="password" name="password" className="form-control" id="password" />
+                </div>
+                <input type="submit" value="Acceder" className="button-green" />
+            </form>
+            <p>¿No tienes cuenta? <Link to="register"><span className="primary-color">Regístrate</span></Link></p>
+        </div>
+    </>);
 };
 
 export default Login;
